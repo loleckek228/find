0bce14b0087bca007727b0a6269ee183879dc79f
loleckek228 <george-valiev@yandex.ru>
2019-03-12 03:35:47 +0300

    ))

--- src/testFiles.java
+++ src/testFiles.java
@@ -9,13 +9,13 @@ public class testFiles {
     @Test
     public void test() {
         Find file = new Find("test/directory1", false);
-        assertEquals(new File("test/directory1/file1"), file.find("file1"));
+        assertEquals(new File("test/directory1/file1.txt"), file.find("file1.txt"));
     }
 
     @Test
     public void test1() {
         Find file = new Find("test", true);
-        assertEquals(new File("test/directory1/file1"), file.find("file1"));
+        assertEquals(new File("test/directory1/file1.txt"), file.find("file1.txt"));
 
     }
 }
